6186cfef2b805c2a623dce4fcebbea2bb8aad397
mdtmc <miles.cranmer@gmail.com>
2015-09-12 13:30:49 -0400

    added pretty much most things. next would be to increase usability, slight improvements on search functionality

--- searches.py
+++ searches.py
@@ -1,16 +1,71 @@
-def search(self, user_input):
-		searchterms = user_input + ' site:stackoverflow.com'
-		sublime.status_message("Searching: " + searchterms)
-		try:
-			myParser = MyHTMLParser()
-			sublime.status_message("searching...")
-			gs = GoogleSearch(searchterms)
-			gs.results_per_page = 10
-			results = gs.get_results()
-			for res in results:
-				url = res.url.encode('utf8')
-				sublime.status_message(url)
-				html = urllib2.urlopen(url).read()
-				myParser.feed(html)
-		except SearchError, e:
-			 sublime.message_dialog("Search failed: %s" % e)
+import urllib2
+from HTMLParser import HTMLParser
+from search import GoogleSearch, SearchError
+
+class MyHTMLParser(HTMLParser):
+	code_flag = 0
+	curr_snip = ''
+	snips = []
+	def handle_starttag(self,tag,attrs):
+		if tag == 'div':
+			for attr in attrs:
+				if attr == ('class','answer accepted-answer'):
+					self.code_flag = 1
+				elif attr == ('class','answer'):
+					self.code_flag = 0
+		if self.code_flag==1 and tag == 'pre':
+			self.code_flag = 2
+		if self.code_flag==2 and tag == 'code':
+			self.code_flag = 3
+	def handle_data(self,data):
+		if self.code_flag>2:
+			self.curr_snip += data
+			#print data
+	def handle_endtag(self,tag):
+		if tag == 'code':
+			if len(self.curr_snip) > 0:
+				self.snips.append(self.curr_snip)
+				self.curr_snip = ''
+			self.code_flag = 1
+
+
+
+
+
+
+#headers for page request
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+
+def search(user_input):
+	searchterms = user_input + ' site:stackoverflow.com'
+	myParser = MyHTMLParser()
+	print "searching "+ user_input
+	try:
+		print("searching...")
+		gs = GoogleSearch(searchterms)
+		gs.results_per_page = 50
+		results = gs.get_results()
+		for res in results:
+			url = res.url.encode('utf8')
+			print(url)
+			req = urllib2.Request(url,headers=hdr)
+			try:
+				page = urllib2.urlopen(req)
+				html = page.read()
+				html_fixed = html.replace('&gt;',' ')
+				html_fixed = html.replace('...',' ')
+				myParser.feed(html_fixed)
+			except urllib2.HTTPError,e:
+				print e.fp.read()
+	except SearchError, e:
+		 print("Search failed: %s" % e)
+	for x in myParser.snips:
+		print x
+
+
+search("python how to basic for loop")
--- stack.py
+++ stack.py
@@ -7,16 +7,48 @@ sys.path.append(os.path.join(os.path.dirname(__file__),"xgoogle"))#/site-package
 
 from search import GoogleSearch, SearchError
 
-
-
 class MyHTMLParser(HTMLParser):
-	def starttag(self,tag,attrs):
+	code_flag = 0
+	curr_snip = ''
+	snips = []
+	def handle_starttag(self,tag,attrs):
 		if tag == 'div':
-			sublime.status_message(attrs)
+			for attr in attrs:
+				if attr == ('class','answer accepted-answer'):
+					self.code_flag = 1
+				elif attr == ('class','answer'):
+					self.code_flag = 0
+		if self.code_flag==1 and tag == 'pre':
+			self.code_flag = 2
+		if self.code_flag==2 and tag == 'code':
+			self.code_flag = 3
+	def handle_data(self,data):
+		if self.code_flag>2:
+			self.curr_snip += data
+			#print data
+	def handle_endtag(self,tag):
+		if tag == 'code':
+			if len(self.curr_snip) > 0:
+				self.snips.append(self.curr_snip)
+				self.curr_snip = ''
+			self.code_flag = 1
+
+
+#headers for page request
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
 		
 
 class SearchtextCommand(sublime_plugin.TextCommand):
+	snips = ['lel']
+	myParser = MyHTMLParser()
+	editor = 0
 	def run(self,edit):
+		self.editor = edit
 		language = self.view.settings().get('syntax').split('/')[1]
 		sels = self.view.sel()
 		#if some text selected:
@@ -37,18 +69,33 @@ class SearchtextCommand(sublime_plugin.TextCommand):
 		searchterms = user_input + ' site:stackoverflow.com'
 		sublime.status_message("Searching: " + searchterms)
 		try:
-			myParser = MyHTMLParser()
 			sublime.status_message("searching...")
 			gs = GoogleSearch(searchterms)
 			gs.results_per_page = 10
 			results = gs.get_results()
+			sublime.status_message("downloading html")
 			for res in results:
 				url = res.url.encode('utf8')
-				sublime.status_message(url)
-				html = urllib2.urlopen(url).read()
-				myParser.feed(html)
+				print(url)
+				req = urllib2.Request(url,headers=hdr)
+				try:
+					page = urllib2.urlopen(req)
+					html = page.read()
+					html_fixed = html.replace('&gt;',' ')
+					html_fixed = html.replace('...',' ')
+					self.myParser.feed(html_fixed)
+				except urllib2.HTTPError,e:
+					print e.fp.read()
 		except SearchError, e:
-			 sublime.message_dialog("Search failed: %s" % e)
+			sublime.message_dialog("Search failed: %s" % e)
+			sublime.status_message("")
+		self.snips = self.myParser.snips
+		for x in self.snips:
+			if sublime.ok_cancel_dialog(x):
+				self.view.insert(self.editor,
+					self.view.sel()[0].begin(),x)
+				break
+
 
 
 
