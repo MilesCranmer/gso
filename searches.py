3addf7c820b5b86a8c3b3d4c61b6c45971d3d1d5
mdtmc <miles.cranmer@gmail.com>
2015-09-13 00:07:13 -0400

    trying to get searches better

--- searches.py
+++ searches.py
@@ -1,37 +1,57 @@
 import urllib2
 from HTMLParser import HTMLParser
 from search import GoogleSearch, SearchError
+from easygui import msgbox
 
 class MyHTMLParser(HTMLParser):
 	code_flag = 0
 	curr_snip = ''
+	curr_snips = []
+	curr_comment = ''
 	snips = []
+	divs = 0
+	answers = 0
 	def handle_starttag(self,tag,attrs):
 		if tag == 'div':
+			if self.divs > 0:
+				self.divs += 1
 			for attr in attrs:
 				if attr == ('class','answer accepted-answer'):
+					self.divs = 1
 					self.code_flag = 1
-				elif attr == ('class','answer'):
-					self.code_flag = 0
+					self.answers += 1
+				elif self.answers < 2 and attr == ('class', 'answer'):
+					self.divs = 1
+					self.code_flag = 1
+					self.answers += 1
+		#if self.code_flag == 1 and tag == 'td' and\
+		#	attrs[0] ==('class','answercell'):
 		if self.code_flag==1 and tag == 'pre':
 			self.code_flag = 2
 		if self.code_flag==2 and tag == 'code':
 			self.code_flag = 3
 	def handle_data(self,data):
+		if self.divs>0:
+			self.curr_comment += data
 		if self.code_flag>2:
 			self.curr_snip += data
 			#print data
 	def handle_endtag(self,tag):
-		if tag == 'code':
-			if len(self.curr_snip) > 0:
-				self.snips.append(self.curr_snip)
+		if tag == 'div' and self.divs > 0:
+			self.divs -= 1
+			#done!
+			if self.divs == 0:
+				if len(self.curr_snips) > 0:
+					self.snips.append([self.curr_snips,self.curr_comment])
+				self.curr_comment = ''
+				self.curr_snips = []
 				self.curr_snip = ''
+				self.code_flag = 0
+		if tag == 'code':
+			if len([x for x in self.curr_snip if x.isalpha()])>=4:
+				self.curr_snips.append(self.curr_snip)
+			self.curr_snip = ''
 			self.code_flag = 1
-
-
-
-
-
 
 #headers for page request
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
@@ -42,9 +62,9 @@ hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML,
        'Connection': 'keep-alive'}
 
 def search(user_input):
-	searchterms = user_input + ' site:stackoverflow.com'
 	myParser = MyHTMLParser()
-	print "searching "+ user_input
+	searchterms = user_input + ' site:stackoverflow.com'
+	print("Searching: " + searchterms)
 	try:
 		print("searching...")
 		gs = GoogleSearch(searchterms)
@@ -52,20 +72,25 @@ def search(user_input):
 		results = gs.get_results()
 		for res in results:
 			url = res.url.encode('utf8')
-			print(url)
+			print "downloading html from", url
 			req = urllib2.Request(url,headers=hdr)
 			try:
 				page = urllib2.urlopen(req)
 				html = page.read()
 				html_fixed = html.replace('&gt;',' ')
-				html_fixed = html.replace('...',' ')
+				html_fixed = html_fixed.replace('...',' ')
+				myParser.answers = 0
 				myParser.feed(html_fixed)
+				snips = myParser.snips
+				msgbox('sup mfo')
+				for x in snips:
+					print x[0]
+				myParser.snips = []
+
 			except urllib2.HTTPError,e:
 				print e.fp.read()
 	except SearchError, e:
-		 print("Search failed: %s" % e)
-	for x in myParser.snips:
-		print x
+		print("Search failed: %s" % e)
 
 
-search("python how to basic for loop")
+search("haskell how to convert string to integer")
