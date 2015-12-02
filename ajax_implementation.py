46704e7b3a67643c18a77bcae209cefe5e043f7e
mdtmc <miles.cranmer@gmail.com>
2015-12-02 18:06:44 -0500

    Changed ajax script to yield urls

--- ajax_implementation.py
+++ ajax_implementation.py
@@ -1,17 +1,15 @@
 #for reading and parsing the Google search data
 import urllib
 import simplejson
-import sys
 
-while len(sys.argv) < 2:
-    sys.argv.append('Example search')
-
-
-query = urllib.urlencode({'q':sys.argv[1]})
-url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s'\
+def get_urls(search):
+    query = urllib.urlencode({'q':search})
+    url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s'\
     % (query)
-search_results = urllib.urlopen(url)
-json = simplejson.loads(search_results.read())
-results = json['responseData']['results']
-for i in results:
-    print i['title'] + ': ' + i['url']
+    search_results = urllib.urlopen(url)
+    json = simplejson.loads(search_results.read())
+    results = json['responseData']['results']
+    for i in results:
+        yield i['url']
+    
+
