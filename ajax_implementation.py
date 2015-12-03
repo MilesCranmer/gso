81bae2279ba0fbe652d993ae82a7153e56c6b321
mdtmc <miles.cranmer@gmail.com>
2015-12-02 19:50:18 -0500

    variable number of search results

--- ajax_implementation.py
+++ ajax_implementation.py
@@ -2,14 +2,16 @@
 import urllib
 import simplejson
 
-def get_urls(search):
-    query = urllib.urlencode({'q':search})
-    url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s'\
-    % (query)
-    search_results = urllib.urlopen(url)
-    json = simplejson.loads(search_results.read())
-    results = json['responseData']['results']
-    for i in results:
-        yield i['url']
+def get_urls(search, num):
+    for x in range(int(num/4)):
+        query = urllib.urlencode({'q':search})
+        url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s'\
+            % (query)
+        url += '&start='+str(x*4)
+        search_results = urllib.urlopen(url)
+        json = simplejson.loads(search_results.read())
+        results = json['responsedata']['results']
+        for i in results:
+            yield i['url']
     
-
+print [x for x in get_urls("TEST",10)]
