a41723a80ea0e1a04025c9ab9ef841b9e6f5d9a2
MilesCranmer <miles.cranmer@gmail.com>
2017-06-02 11:29:26 +1000

    Working initial search function, straight from google repo

--- example.py
+++ example.py
@@ -3,18 +3,20 @@ import os
 from googleapiclient.discovery import build
 
 my_api_key = os.environ["GOOGLE_KEY"]
-my_cse_id = "003962226882031433174"
+my_cse_id = "003962226882031433174:qk7rs-ca-bi"
 
-def google_search(search_term, api_key, cse_id, **kwargs):
-    service = build("customsearch", "v1", developerKey=api_key)
-    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
-    return res['items']
+def main():
+    # Build a service object for interacting with the API. Visit
+    # the Google APIs Console <http://code.google.com/apis/console>
+    # to get an API key for your own application.
+    service = build("customsearch", "v1",
+            developerKey=my_api_key)
 
-results = google_search(
-            'How to set up linux',
-            my_api_key,
-            my_cse_id,
-            num=10)
+    res = service.cse().list(
+            q='How to set up linux',
+            cx=my_cse_id,
+            ).execute()
+    pprint.pprint(res)
 
-for result in results:
-    pprint.pprint(result)
+if __name__ == '__main__':
+    main()
