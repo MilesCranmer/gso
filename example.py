40be8f7c183c404f82cbc916c8b892753440fabd
MilesCranmer <miles.cranmer@gmail.com>
2017-06-02 18:31:04 +1000

    Set up to load in questions

--- example.py
+++ example.py
@@ -9,4 +9,13 @@ import stackexchange
 SE_KEY = os.environ["SE_KEY"]
 
 question_id = 895371
-so = stackexchange.Site(stackexchange.StackOverflow)
+
+so = stackexchange.Site(
+    stackexchange.StackOverflow,
+    app_key=SE_KEY)
+
+so.impose_throttling = True
+so.throttle_stop = False
+
+question = so.question(question_id)
+print question.answers
