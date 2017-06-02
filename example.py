56e11c3df02874867626551534693b488db82fb7
MilesCranmer <miles.cranmer@gmail.com>
2017-06-02 23:50:04 +1000

    Create wrapper tag for SO html

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 import os
 import pickle as pkl
+from lxml import etree
 from gso import load_up_answers, load_up_questions
 
 #for result in load_up_questions("How to write a bubble sort", "python"):
@@ -15,4 +16,9 @@ html_dump = []
 with open("html_dump.pkl", 'rb') as myfile:
     html_dump = pkl.load(myfile)
 
-print html_dump
+def wrapper_tag(xml_string):
+    xml_string = "<root>"+xml_string+"</root>"
+    return xml_string
+
+root = etree.fromstring(wrapper_tag(html_dump[0][1]))
+print etree.tostring(root)
